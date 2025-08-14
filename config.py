6e8d96ba969f497aecf581089886f202520d817e
mister570 <vkm16463@gmail.com>
2025-08-14 19:16:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -83,10 +83,10 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://files.catbox.moe/uawp0n.gif"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/3llj9d.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/f1k78w.png"
 )
-PLAYLIST_IMG_URL = "https://files.catbox.moe/3llj9d.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/3llj9d.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/k0cxs3.png"
+STATS_IMG_URL = "https://files.catbox.moe/67cuia.gif"
 TELEGRAM_AUDIO_URL = "https://files.catbox.moe/3llj9d.jpg"
 TELEGRAM_VIDEO_URL = "https://files.catbox.moe/3llj9d.jpg"
 STREAM_IMG_URL = "https://files.catbox.moe/3llj9d.jpg"
